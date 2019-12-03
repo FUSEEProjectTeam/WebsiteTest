8cd55be24d0d83431bbca5068b27a0a1648fc661
Alexander Scheurer <mail@aspepex.net>
2019-12-03 16:19:28 +0100

    moved docs folder back to root

--- Build.cmd
+++ Build.cmd
@@ -1,10 +1,11 @@
 @echo off
 setlocal
 
-SET BUILDPATH=".\docs"
+SET ROOT=%CD%
+SET BUILDPATH="%CD%\docs"
 SET BASEURL="https://fuseeprojectteam.github.io/WebsiteTest/"
 
-SET HUGO=".\bin\Hugo\hugo.exe"
+SET HUGO="%CD%\bin\Hugo\hugo.exe"
 
 %HUGO% --destination %BUILDPATH% --theme=beautifulhugo --baseURL %BASEURL% --cleanDestinationDir -s .\src\Hugo
 
