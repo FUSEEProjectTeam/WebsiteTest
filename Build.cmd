fad9220d8c47d4fe8baa9326e63227af5915ba98
Alexander Scheurer <mail@aspepex.net>
2019-12-04 13:11:56 +0100

    basic config for docfx

--- Build.cmd
+++ Build.cmd
@@ -2,11 +2,19 @@
 setlocal
 
 SET ROOT=%CD%
-SET BUILDPATH="%CD%\docs"
-SET BASEURL="https://fuseeprojectteam.github.io/WebsiteTest/"
+SET OUTPUTPATH=%CD%\docs
+SET BASEURL=https://fuseeprojectteam.github.io/WebsiteTest/
 
+SET FUSEESRC=../Fusee/src
+
+SET DOCFX="%CD%\bin\DocFX\docfx.exe"
 SET HUGO="%CD%\bin\Hugo\hugo.exe"
 
-%HUGO% --destination %BUILDPATH% --theme=beautifulhugo --baseURL %BASEURL% --cleanDestinationDir -s .\src\Hugo
+%DOCFX% "%CD%\src\DocFX\docfx.json" -o "%OUTPUTPATH%"
+
+rem %HUGO% --destination %OUTPUTPATH% --theme=beautifulhugo --baseURL %BASEURL% --cleanDestinationDir -s .\src\Hugo
+
+
+
 
 endlocal
