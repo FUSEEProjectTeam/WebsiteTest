374724867b5986778406850efc98fd76cd5560e0
Alexander Scheurer <mail@aspepex.net>
2020-11-23 12:40:46 +0100

    DocFX and Build.cmd update

--- Build.cmd
+++ Build.cmd
@@ -1,19 +1,16 @@
 @echo off
 setlocal
 
-SET ROOT=%CD%
 SET OUTPUTPATH=%CD%\docs
-SET BASEURL=https://fuseeprojectteam.github.io/WebsiteTest/
-
-SET FUSEESRC=../Fusee/src
-
 SET DOCFX="%CD%\bin\DocFX\docfx.exe"
-SET HUGO="%CD%\bin\Hugo\hugo.exe"
+
+rd /s /q %OUTPUTPATH%
+mkdir %OUTPUTPATH%
+
+echo fusee3d.org > %OUTPUTPATH%\CNAME
+
 
 %DOCFX% "%CD%\src\DocFX\docfx.json" -o "%OUTPUTPATH%"
-
-rem %HUGO% --destination %OUTPUTPATH% --theme=beautifulhugo --baseURL %BASEURL% --cleanDestinationDir -s .\src\Hugo
-
 
 
 
